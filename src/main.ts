68d0bf89a4afce59b4a943c45b0fb833171eca54
Artem <artem.mikheev.git@gmail.com>
2024-04-17 10:27:18 +0300

    connect database

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
+import { AppModule } from '@/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = await app.get<ConfigService>(ConfigService);
+  const port = configService.get<number>('API_PORT', 3000);
+
+  await app.listen(port, () => {
+    Logger.log(`Server started on http://localhost:${port}`, 'Main');
+  });
 }
 bootstrap();
