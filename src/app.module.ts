360c990ea1b125cc3cd6babbf3776d6b66f0648f
Artem <artem.mikheev.git@gmail.com>
2024-04-17 11:59:21 +0300

    create user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeormModule } from '@/typeorm';
 import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), TypeormModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeormModule,
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
